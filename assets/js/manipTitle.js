7ed75c48852a0530d68d9b1455fff537deb16dd7
Ilman123 <ilman.xrpl2@gmail.com>
2021-12-25 22:36:25 +0700

    manipulation Title

---
+++ assets/js/manipTitle.js
@@ -0,0 +1,13 @@
+// mengganti title sesuai content yang di click dengan menggunakan jquery
+$("#homeClick").click(function () {
+  document.getElementById("titlenya").innerHTML = "Home Ilman - Portofolio";
+});
+$("#aboutClick").click(function () {
+  document.getElementById("titlenya").innerHTML = "About Ilman - Portofolio";
+});
+$("#projectsClick").click(function () {
+  document.getElementById("titlenya").innerHTML = "Projects Ilman - Portofolio";
+});
+$("#contactClick").click(function () {
+  document.getElementById("titlenya").innerHTML = "Contact Ilman - Portofolio";
+});
